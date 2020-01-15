2033bb09f46f341ed5c326cf65a1603475cc5a29
Nikske <wolfvdd@gmail.com>
2020-01-15 12:08:48 +0100

    image and moves are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@
 let input = document.getElementById("pokemon");
 
 document.getElementById("run").addEventListener("click", function() {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + input.value).then((response) => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase()).then((response) => {
         return response.json();
     })
         .then((data) => {
             console.log(data);
-
+            // Image
+            document.getElementById("pokeimg").src = data['sprites']['front_default'];
+            // Moves
+            for (let i = 0; i<4; i++) {
+                document.getElementById("pokemoves").innerHTML += "<li>" + data['moves'][i]['move']['name'] + "</li>"
+            }
         })
 });
