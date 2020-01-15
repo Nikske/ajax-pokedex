4abf6d0a147f84f29da7aa70ae1123007ce267fa
Nikske <wolfvdd@gmail.com>
2020-01-15 14:48:00 +0100

    fixing moves

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@
 
 let input = document.getElementById("pokemon");
 
+function randomNumber(number) {
+
+}
+
 document.getElementById("run").addEventListener("click", function() {
     fetch("https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase()).then((response) => {
         return response.json();
@@ -11,8 +15,13 @@ document.getElementById("run").addEventListener("click", function() {
             // Image
             document.getElementById("pokeimg").src = data['sprites']['front_default'];
             // Moves
+            let moveList = [];
             for (let i = 0; i<4; i++) {
-                document.getElementById("pokemoves").innerHTML += "<li>" + data['moves'][i]['move']['name'] + "</li>"
+                moveList.push(data['moves'][i]['move']['name']);
             }
+            document.getElementById("move1").innerHTML= moveList[0];
+            document.getElementById("move2").innerHTML= moveList[1];
+            document.getElementById("move3").innerHTML= moveList[2];
+            document.getElementById("move4").innerHTML= moveList[3];
         })
 });
